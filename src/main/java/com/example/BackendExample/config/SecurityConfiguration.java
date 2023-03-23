e369509f5486a25e76be63947c1fd5041bf3512a
win 10 <akashgusain57@gmail.com>
2023-03-23 22:27:13 +0530

    Authorization enabled

--- src/main/java/com/example/BackendExample/config/SecurityConfiguration.java
+++ src/main/java/com/example/BackendExample/config/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.example.BackendExample.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
@@ -18,12 +19,21 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("blah")
                 .password("blah")
-                .roles("USER");
+                .roles("ADMIN");
 
     }
 
     @Bean
     public PasswordEncoder getPasswordEncoder(){
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/admin").hasAnyRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER")
+                .antMatchers("/","static/css","static/js").permitAll()
+        .and().formLogin();
     }
 }
---
+++ src/main/java/com/example/BackendExample/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.example.BackendExample.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String all(){
+        return "Welcome all";
+    }
+
+    @GetMapping("/user")
+    public String user(){
+        return "Welcome user";
+    }
+    @GetMapping("/admin")
+    public String admin(){
+        return "Welcome admin";
+    }
+}
